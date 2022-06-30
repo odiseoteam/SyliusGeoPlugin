242dc2276f4fc88005e52b381c7499accdbcadff
bigboss <damian@odiseo.com.ar>
2022-06-30 14:22:56 -0300

    fix tests

--- tests/Behat/Context/Ui/Shop/GeoContext.php
+++ tests/Behat/Context/Ui/Shop/GeoContext.php
@@ -5,13 +5,23 @@ declare(strict_types=1);
 namespace Tests\Odiseo\SyliusGeoPlugin\Behat\Context\Ui\Shop;
 
 use Behat\Behat\Context\Context;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 final class GeoContext implements Context
 {
+    private Session $session;
+
+    public function __construct(
+        Session $session
+    ) {
+        $this->session = $session;
+    }
+
     /**
      * @When I shop from Australia
      */
     public function iShopFromAustralia()
     {
+        $this->session->set('countryCode', 'AU');
     }
 }
